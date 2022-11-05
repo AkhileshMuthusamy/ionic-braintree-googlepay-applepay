89aa17dd99d087bd7d77850321884cd08d31ce67
Akhilesh Muthusamy <akhilesh.muthusamy@rideco.com>
2022-11-05 11:59:58 -0400

    Add cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,5 @@
+ionic cordova platform remove android
+ionic cordova plugin remove cordova-plugin-braintree-googlepay-applepay
+
+ionic cordova plugin add ../cordova-plugin-braintree-googlepay-applepay --link
+ionic cordova platform add android
